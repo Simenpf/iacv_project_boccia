a93cb0024732e938568013676921d08c15f63ca6
Simen Piene Fløtaker <Simen.flotaker@gmail.com>
2022-02-06 13:39:07 +0100

    Calibration added

--- source/camera_calibration.py
+++ source/camera_calibration.py
@@ -1,51 +1,111 @@
 import cv2 as cv
 import numpy as np
-import glob
 import imutils
+from rectify_court import get_court_homography
 
-# Chessboard
+
+# Function for retrieving camera intrinsics based on a chessboard calibration video
+def getCameraIntrinsics(video,board_size,square_size):
+    # Declare points to be used for calibration
+    true_pts = []
+    img_pts = []
+
+    # Create a row vector of all zeros
+    true_board_corners = np.zeros((1, board_size[0] * board_size[1], 3), np.float32) 
+
+    # Edit the row vector to contain all corners of the true chessboard
+    true_board_corners[0,:,:2] = np.mgrid[0:board_size[0], 0:board_size[1]].T.reshape(-1, 2)
+    true_board_corners *= square_size
+
+    # Specify criteria for subpixel optimization
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.01)
+
+    # Load all calibration video and find chessboard corners in the frames
+    num_frames = int(video.get(cv.CAP_PROP_FRAME_COUNT))
+    while True:
+        # Select cirka 30 frames to be used for calibration
+        success, img = video.read()
+        for i in range(0,num_frames//30):
+            success, img = video.read()
+
+        # While there are still frames
+        if  success:
+            gray_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+            # Search for corners in grayscale image
+            success, image_board_corners = cv.findChessboardCorners(gray_img, board_size, cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_FAST_CHECK + cv.CALIB_CB_NORMALIZE_IMAGE)
+            if success:
+                # Add repeat of true corners as a match to the new image corners
+                true_pts.append(true_board_corners)
+
+                # Optimize corners found in image
+                image_board_corners = cv.cornerSubPix(gray_img, image_board_corners, (11,11), (-1,-1), criteria)
+
+                # Add the new image corners 
+                img_pts.append(image_board_corners)
+
+                # Draw the found corners for visualization
+                img = cv.drawChessboardCorners(img, board_size, image_board_corners, success)
+
+            # Show the image for manual supervision
+            window_img = imutils.resize(img,width=1000)
+            cv.imshow("Calibration",window_img)
+            cv.imwrite("checkerboard.jpg",img)
+            cv.waitKey(0)
+        else:
+            break
+    cv.destroyAllWindows()
+
+    # Get camera intrinsics
+    _, K, dist, _, _ = cv.calibrateCamera(true_pts, img_pts, gray_img.shape[::-1], None, None)
+    return K, dist
+
+# Function for retrieving the projection matrix based on camera intrinsics and a set of known points
+def getCameraProjectionMatrix(camera_intrinsics, distortion_coefficients, true_pts, img_pts):
+   _, rvec, tvec = cv.solvePnP(true_pts, img_pts, camera_intrinsics, distortion_coefficients)
+   R, _ = cv.Rodrigues(rvec)
+
+   RT = np.concatenate((R,tvec),axis=1)
+   P = camera_intrinsics.dot(RT)
+   P /= P[2,3]
+   return P
+
+
+# Testing the functions
+#######################
+# Define chessboard
 board_size = (9,7)
 square_size = 2
-true_pts = []
-img_pts = []
 
-# Create a row vector of all zeros
-true_board_corners = np.zeros((1, board_size[0] * board_size[1], 3), np.float32) 
+# Load media
+video = cv.VideoCapture('../media/calibration_video_camera2.mp4')
+frame = cv.imread("../media/bocce_game_camera2_reference_frame.jpg")
 
-# Edit the row vector to contain all corners of the true chessboard
-true_board_corners[0,:,:2] = np.mgrid[0:board_size[0], 0:board_size[1]].T.reshape(-1, 2)
-true_board_corners *= square_size
+# Define known points in image and world frame
+court_width = 390
+court_length = 202
+court_ratio = court_width/court_length
+_,_,corners_selected = get_court_homography(frame,court_ratio,1000,0)
+corners_selected = corners_selected[0]
+corners_actual = np.array([[0, 0, 0],[0, 202, 0],[court_width, court_length, 0],[court_width, 0, 0]],dtype=np.float32)
 
-# Specify criteria for subpixel optimization
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.01)
+# Retrieve intrinsics and extrinsics
+K, dist = getCameraIntrinsics(video,board_size,square_size)
+P = getCameraProjectionMatrix(K,dist,corners_actual,corners_selected)
 
 
-# Load all calibration images and find chessboard corners in them
-images = glob.glob('../media/calibration_images_camera_1/*.jpg')
-for image in images:
-    img = cv.imread(image)
-    gray_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+# Draw a known point in the three-dimensional world frame on it projected position
+X = [390+37,-202,110,1]
+x = P.dot(X)
+x /= x[2]
 
-    # Search for corners in grayscale image
-    success, image_board_corners = cv.findChessboardCorners(gray_img, board_size, cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_FAST_CHECK + cv.CALIB_CB_NORMALIZE_IMAGE)
-    if success:
-        # Add repeat of true corners as a match to the new image corners
-        true_pts.append(true_board_corners)
+frame = cv.imread("../media/bocce_game_camera2_reference_frame.jpg")
+frame = cv.circle(frame,(round(x[0]),round(x[1])),10,(0,0,255),-1)
+frame_small = imutils.resize(frame,width=800)
+cv.imshow("",frame_small)
+cv.waitKey(0)
 
-        # Optimize corners found in image
-        image_board_corners = cv.cornerSubPix(gray_img, image_board_corners, (11,11), (-1,-1), criteria)
 
-        # Add the new image corners 
-        img_pts.append(image_board_corners)
 
-        # Draw the found corners for visualization
-        img = cv.drawChessboardCorners(img, board_size, image_board_corners, success)
-    
-    # Show the image for manual supervision
-    window_img = imutils.resize(img,width=1000)
-    cv.imshow("Calibration",window_img)
-    cv.waitKey(0)
-cv.destroyAllWindows()
 
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(true_pts, img_pts, gray_img.shape[::-1], None, None)
-print(mtx)
+
--- source/main.py
+++ source/main.py
@@ -17,9 +17,10 @@ def estimate_ball_positions(pos,new_pos):
         
 
 # Load video feed and first frame
-video = cv.VideoCapture('../media/trimmed_game3_no_ornaments.mp4')
+video = cv.VideoCapture('../media/bocce_game_camera1_trimmed.mp4')
 #video = cv.VideoCapture(0) # For testing with webcam
-_, frame = video.read()
+#_, frame = video.read()
+frame = cv.imread("../media/bocce_game_camera1_screenshot.jpg")
 
 # Frame resolution
 frame_width  = frame.shape[1]
@@ -29,7 +30,7 @@ frame_height = frame.shape[0]
 detection_scaling = 0.5
 
 # For faster tracking some frames can be skipped (set to zero for tracking all frames)
-skip_frames = 0
+skip_frames = 10
 
 # Screen resolution
 #screen_width  = int(input("Screen width (pixels): "))
@@ -44,14 +45,14 @@ win_height = round(screen_height*0.5)
 # Court size
 #court_width  = int(input("Court width (mm): "))
 #court_length = int(input("Court length (mm): "))
-court_length = 2210
-court_width = 1500
+court_length = 202
+court_width = 390
 court_ratio = court_width/court_length
 
 
 # Get court rectifying homography
 padding = 0.1 # The amount of image used from outside court (in fraction of court length)
-H, court_mask = get_court_homography(frame, court_ratio, win_width, padding)
+H, court_mask, _ = get_court_homography(frame, court_ratio, win_width, padding)
 
 # Perform game tracking
 pos_out_of_court = (-1,-1)
@@ -67,7 +68,7 @@ while True:
     # While there are frames in video-feed, run game-tracking
     if success:
         # Mask out court (+ padding), so detection is only done on court
-        frame = cv.bitwise_and(frame, frame ,mask = court_mask)
+        #frame = cv.bitwise_and(frame, frame ,mask = court_mask)
 
         # Track balls
         masks, new_ball_positions = detect_balls(frame, detection_scaling)
@@ -84,8 +85,8 @@ while True:
         rectified_masks = cv.warpPerspective(masks, H, (frame_width, round(frame_width*court_ratio)))
 
         # Display results
-        overview = np.concatenate((rectified_frame, rectified_masks), axis=1)
-        #overview = np.concatenate((frame, masks), axis=1)
+        #overview = np.concatenate((rectified_frame, rectified_masks), axis=1)
+        overview = np.concatenate((frame, masks), axis=1)
         overview = imutils.resize(overview, width=win_width)
         cv.imshow("Rectified Court (press enter to exit...)", overview)
 
